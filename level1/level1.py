e8899b476999885963497895563708f8202a1c7c
swaylakh <swaylakh@gmail.com>
2024-08-27 11:35:14 +0530

    some changes

--- level1/level1.py
+++ level1/level1.py
@@ -1,14 +1,14 @@
-import google.generativeai as genai ##geminiapi
-import os ## for api privacy
-import json ## to get output in .json format
-import time ## to get time sent and recieved
+import google.generativeai as genai 
+import os
+import json 
+import time 
 
-genai.configure(api_key=os.getenv("API_KEY"))
+genai.configure(api_key=os.getenv("API_KEY"))  ##configuring and calling the model
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-def getResponse(prompt):
+def getResponse(prompt): ##function to get response from gemini using api
     time_sent = int(time.time())
-    response = model.generate_content(prompt + " in one line")
+    response = model.generate_content(prompt + " in one line") ##helps in reducing token used and get a small answer
     message = response.text.replace('\n', ' ').strip()
     time_recieved = int(time.time())
     return {
@@ -19,9 +19,8 @@ def getResponse(prompt):
         "Source": "Gemini"
     }
 
-def textInput(input_file, output_file):
+def textInput(input_file, output_file): ##function to read lines from text file and create an output json file
     output = []
-
     with open(input_file, 'r') as file:
         prompts = file.readlines()
         for prompt in prompts:
@@ -33,4 +32,7 @@ def textInput(input_file, output_file):
     with open(output_file, 'w') as file:
         json.dump(output, file, indent=2)
 
-textInput('input.txt', 'output.json')
+    print("Output file created")
+
+
+textInput('input.txt', 'output.json') ##calling the function
