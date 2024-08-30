85edb526039896e2227931d865bc07154d9aceff
Swayam Lakhotia <72654570+swaylakh@users.noreply.github.com>
2024-08-31 03:49:50 +0530

    Update start.sh

--- level2&3/start.sh
+++ level2&3/start.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-# Function to start the server and wait until it's ready
+
 start_server() {
     echo "Starting the server..."
-    python3 server.py &  # Redirect output to server.log
-    SERVER_PID=$!  # Store the PID of the server process
-
+    python3 server.py &
+    SERVER_PID=$! 
     echo "Waiting for the server to start..."
-    sleep 5  # Increase if necessary to give the server time to initialize
+    sleep 5  
 }
 
-# Function to start the client
 start_client() {
     echo "Starting the client..."
-    python3 client.py  # Redirect output to client.log
+    python3 client.py  
 }
 
-# Function to stop the server
 stop_server() {
     echo "Stopping the server..."
     kill $SERVER_PID
-    wait $SERVER_PID  # Wait for the server process to terminate
+    wait $SERVER_PID 
 }
 
 
